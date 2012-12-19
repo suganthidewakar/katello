47301b6ca9dfb3e670130ccd12fa65e927b39fad
Eric D Helms <ehelms@redhat.com>
2012-12-19 16:21:53 -0500

    Rails32 - Removes compass compile that is no longer valid in CI tests.

--- scripts/ci/katello_pull_request_tests.sh
+++ scripts/ci/katello_pull_request_tests.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
 cd src/
-echo "\n"
-echo "********* Stylesheet Compilation Test  ***************"
-echo "RUNNING: RAILS_ENV=development bundle exec compass compile"
-RAILS_ENV=development bundle exec compass compile
-if [ $? -ne 0 ]
-then
-  exit 1
-fi
-
 echo ""
 echo "********* RSPEC Unit Tests ****************"
 psql -c "CREATE USER katello WITH PASSWORD 'katello';" -U postgres
